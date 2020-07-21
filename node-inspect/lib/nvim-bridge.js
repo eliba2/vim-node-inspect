9c6a0e052be34eeb4033be5073e83464f2bd8d75
Eli Babila <elibabila@gmail.com>
2020-07-21 23:25:22 +0300

    better startup connection to bridge.

--- node-inspect/lib/nvim-bridge.js
+++ node-inspect/lib/nvim-bridge.js
@@ -12,6 +12,8 @@ class NvimBridge {
 		this.client = null;
 		this.server = null;
 		this.callback = null;
+		this.initiated = false;
+		this.message_queue = [];
 	}
 
 	send(msg) {
@@ -27,12 +29,21 @@ class NvimBridge {
 
 	setEventCallback(callback) {
 		this.callback = callback;
+		// process any messages in the queue
+		while (this.message_queue.length) {
+			let message = this.message_queue.pop();
+			this.callback(message);
+		}
+
 	}
 
 
 	sendInternalEvent(msg) {
 		if (this.callback) {
 			this.callback(msg);
+		}
+		else {
+			this.message_queue.push(msg);
 		}
 	}
 
@@ -41,15 +52,21 @@ class NvimBridge {
 		return new Promise((resolve,reject) => {
 			this.server = net.createServer((client) => {
 				this.client = client;
-				// I'll resolve only when a client connects
-				resolve();
 				// single client support at this time
 				this.client.on('data', (data) => {
 					//console.log('client says',data.toString(), typeof(data));
+					let message_object = data.toString();
+					let message = JSON.parse(message_object);
+					if (this.initiated == false &&  message.m == 'nd_init') {
+						// that should be the first message from the host
+						this.initiated = true;
+						resolve();
+					}
 					if (this.callback) {
-						let message = data.toString();
-						let json = JSON.parse(message);
-						this.callback(json);
+						this.callback(message);
+					}
+					else {
+						this.message_queue.push(message);
 					}
 				});
 			});
